8ca70aee47e43c5c711b1fa6671c275865db2516
Hello Party <1559567+hello-party@users.noreply.github.com>
2021-09-28 04:05:44 -0700

    Create readme.js

---
+++ js/readme.js
@@ -0,0 +1,11 @@
+var txtFile = new XMLHttpRequest();
+var allText = "";
+txtFile.onreadystatechange = function () {
+    if (txtFile.readyState === XMLHttpRequest.DONE && txtFile.status == 200) {
+        allText = txtFile.responseText;
+        allText = allText.split("\n").join("<br>");
+    }
+     document.getElementById('readme').innerHTML = allText;
+}
+txtFile.open("GET", 'readme.txt', true);
+txtFile.send(null);
