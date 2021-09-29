f52cc59628250f440903ba44ae9b686bb61be071
Hello Party <1559567+hello-party@users.noreply.github.com>
2021-09-29 03:48:05 -0700

    Update readme.js

--- js/readme.js
+++ js/readme.js
@@ -4,12 +4,7 @@ txtFile.onreadystatechange = function () {
     if (txtFile.readyState === XMLHttpRequest.DONE && txtFile.status == 200) {
         allText = txtFile.responseText;
         allText = allText.split("\n").join("<br>")
-         .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
-         .replace(/\*(.*)\*/gim, '<i>$1</i>')
-         .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
-         .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
-         .replace(/\n$/gim, '<br />');
-         }
+    }
      document.getElementById('readme').innerHTML = allText;
 }
 txtFile.open("GET", 'readme.md', true);
